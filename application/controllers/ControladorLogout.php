7962ab455057f5cc511c145e7ed4fd1c9b738227
gomez2019 <47744837+gomez2019@users.noreply.github.com>
2019-05-25 15:53:58 -0500

    Update ControladorLogout.php

--- application/controllers/ControladorLogout.php
+++ application/controllers/ControladorLogout.php
@@ -6,15 +6,7 @@ class ControladorLogout extends CI_Controller {
 
 	public function index(){
 
-		$this->session->unset_userdata('logged_in');
-		$this->session->unset_userdata('correo');
-		$this->session->unset_userdata('nombre');
-		$this->session->unset_userdata('apellidos');
-		$this->session->unset_userdata('sexo');
-		$this->session->unset_userdata('fecha_nacimiento');
-		$this->session->unset_userdata('id');
-		
-
+		$this->session->sess_destroy();  
 
 		$this->session->set_flashdata('usuario_loggedout', 'Has salido  de tu cuenta');
 
